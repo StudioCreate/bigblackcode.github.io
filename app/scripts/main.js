b4e92541c94856572e0b9408d6f4984bb6a089bf
Alex Tselegidis <alextselegidis@gmail.com>
2015-06-25 00:00:47 +0200

    Added simple animations

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,10 +1,28 @@
-(function() {
-    
+(function($) {
+
     'use strict'; 
 
-    $(document).ready(function() {
-        $('.wrapper').fullpage();
-        $('section').css('height', ''); // * section height fix
+    $('#about, #mind-escape').find('p, img').hide();
+    // $('#mind-escape').find('p, img').hide();
+
+    $('.wrapper').fullpage({
+        'onLeave': function(index, nextIndex) {
+            if (index === 1 && nextIndex === 2 && !$('#about img').hasClass('animated')) {
+                
+                setTimeout(function() {
+                    $('#about').find('p, img').fadeIn(500).addClass('animated flip');
+                }, 500);
+
+            } else if (index === 2 && nextIndex === 3) {
+
+                setTimeout(function() {
+                    $('#mind-escape').find('p, img').fadeIn(500).addClass('animated flip');
+                }, 500);
+                
+            }
+        }
     });
 
-})(); 
+    $('section').css('height', ''); // * section js height fix
+
+})(jQuery); 
